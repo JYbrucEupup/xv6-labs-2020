63a4f138936cd915a34a75c5dc1df889edcbe84a
jybruceupup <691800735@qq.com>
2025-10-23 16:19:05 +0800

    完成了system call trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,7 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  p->kama_syscall_trace = 0;//创建新进程的时候， kama_syscall_trace设置为默认值0
   return p;
 }
 
@@ -297,6 +298,7 @@ fork(void)
 
   release(&np->lock);
 
+  np->kama_syscall_trace = p->kama_syscall_trace; //子进程继承父进程的syscall_trace
   return pid;
 }
 
